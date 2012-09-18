95f8c692245edf35849795ee8e235911e7c1491b
Tom Byers <tombye@Tom-Byerss-MacBook-Pro.local>
2012-09-18 21:56:25 +0100

    Remove unused .jade files

--- web.js
+++ web.js
@@ -1,16 +1,3 @@
-//var express = require('express');
-//
-//var app = express.createServer(express.logger());
-//
-//app.get('/', function(request, response) {
-//  response.send('Hello World!');
-//});
-//
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
-
 var express = require('express'),
     app = express.createServer(express.logger()),
     io = require('socket.io').listen(app),
